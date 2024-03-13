54d9cfe0dc5fc5f1e97828c807409620fe81f527
Blackscure <34754398+Blackscure@users.noreply.github.com>
2024-03-13 11:52:36 +0300

    email comnfig created in  settings

--- SaccoProject/settings.py
+++ SaccoProject/settings.py
@@ -59,6 +59,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'Wekesabuyahi@gmail.com'
+EMAIL_HOST_PASSWORD = 'flna kjwh wtjb kfhl'
+
 ROOT_URLCONF = 'SaccoProject.urls'
 
 TEMPLATES = [
